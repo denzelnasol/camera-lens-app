b03d1c4fbf79cbc2e4521ac8388e2a285d1df85c
Denzel Nasol <denzelnasol@users.noreply.github.com>
2020-09-27 19:48:50 -0700

    Initial commit

---
+++ app/src/main/java/sfu/packages/cmpt276a2/MainActivity.java
@@ -0,0 +1,25 @@
+package sfu.packages.cmpt276a2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show());
+    }
+}
